c305878b7b636e008eb263658b029e426e7a757d
elisa <bascongarcia@gmail.com>
2022-08-20 22:43:14 +0200

    routes Donde

--- routes/movies.js
+++ routes/movies.js
@@ -10,7 +10,7 @@ router.get('/', async (req, res, next) => {
     if (movies.length === 0) {
       res.status(200).json({ response: 'hello' })
     } else {
-      res.status(200).json({ data: projects })
+      res.status(200).json({ data: movies })
     }
   } catch (error) {
     next(error);
@@ -25,28 +25,57 @@ router.get('/', async (req, res, next) => {
 // @route   GET /:id
 // @access  Public
 router.get('/:id', async (req, res, next) => {
-
+  const { id } = req.params;
+  try {
+    const movie = await Movie.findById(id);
+    if (!movie) {
+      res.status(404).json({ response: 'Movie not found'});
+    } else {
+      res.status(200).json({ data: movie })
+    }
+  } catch (error) {
+    next (error)
+  }
 });
 
 // @desc    Create a movie
 // @route   POST /
 // @access  Public
 router.post('/', async (req, res, next) => {
-
+  const {title, year, director, duration, synopsis, image } = req.body;
+  try {
+    const movie = await Movie.create({title, year, director, duration, synopsis, image } );
+    res.status(201).json({ data: movie})
+  } catch (error) {
+    next(error);
+  }
 });
 
 // @desc    Edit a movie
 // @route   PUT /:id
 // @access  Public
 router.put('/:id', async (req, res, next) => {
-
+  const { id } = req.params;
+  const {title, year, director, duration, synopsis, image } = req.body;
+  try {
+    const updateMovie = await Movie.findByIdAndUpdate(id, {title, year, director, duration, synopsis, image}, { new: true });
+    res.status(202).json({data: updateMovie})
+  } catch (error) {
+    next(error);
+  }
 });
 
 // @desc    Delete a movie
 // @route   DELETE /:id
 // @access  Public
 router.delete('/:id', async (req, res, next) => {
-
+  const { id } = req.params;
+  try {
+    const deleted = await Movie.findByIdAndDelete(id);
+    res.status(202).json({ data: deleted});
+  } catch (error) {
+    next(error)
+  }
 });
 
 module.exports = router;
